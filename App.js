a5fab71a732dc3344b813a5bc2dcf4eda6f2ed37
Juby94 <31645864+Juby94@users.noreply.github.com>
2017-09-23 21:45:08 +0200

    yoo

--- App.js
+++ App.js
@@ -1,58 +1,86 @@
-import React, {Component} from 'react';
-import { ActivityIndicator, AsyncStorage, StyleSheet, Text, View } from 'react-native';
-import {Router, Scene} from 'react-native-router-flux';
+import React, {
+    Component
+} from 'react';
+import {
+    ActivityIndicator,
+    AsyncStorage,
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+import {
+    Router,
+    Scene
+} from 'react-native-router-flux';
 import * as firebase from 'firebase';
 import Authentication from './components/Authentication';
 import HomePage from './components/HomePage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDGbz6rYVh4IhEHD2as3ckN4UNzMW6S488",
-  authDomain: "marchingfalcon-75e0f.firebaseapp.com",
-  databaseURL: "https://marchingfalcon-75e0f.firebaseio.com",
-  projectId: "marchingfalcon-75e0f",
-  storageBucket: "marchingfalcon-75e0f.appspot.com",
-  messagingSenderId: "744638475270"
+    apiKey: "AIzaSyDGbz6rYVh4IhEHD2as3ckN4UNzMW6S488",
+    authDomain: "marchingfalcon-75e0f.firebaseapp.com",
+    databaseURL: "https://marchingfalcon-75e0f.firebaseio.com",
+    projectId: "marchingfalcon-75e0f",
+    storageBucket: "marchingfalcon-75e0f.appspot.com",
+    messagingSenderId: "744638475270"
 };
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 console.ignoredYellowBox = [
   "Setting a timer"
 ];
 export default class App extends Component {
-  constructor() {
-    super();
-    this.state = { hasToken: false, isLoaded: false };
-  }
-  componentWillMount() {
-    AsyncStorage.getItem('id_token').then((token) => {
-      this.setState({ hasToken: token !== null, isLoaded: true });
-    });
-  }
-  render() {
-    if (!this.state.isLoaded) {
-      return (
-        <ActivityIndicator />
-      )
-    } else {
-      return (
-        <Router>
-          <Scene key='root'>
-            <Scene
-              component={Authentication}
-              hideNavBar={true}
-              initial={!this.state.hasToken}
-              key='Authentication'
-              title='Authentication'
-            />
-            <Scene
-              component={HomePage}
-              hideNavBar={true}
-              initial={this.state.hasToken}
-              key='HomePage'
-              title='Home Page'
-            />
-          </Scene>
-        </Router>
-      );
+    constructor() {
+        super();
+        this.state = {
+            hasToken: false,
+            isLoaded: false
+        };
     }
-  }
+    componentWillMount() {
+        AsyncStorage.getItem('id_token').then((token) => {
+            this.setState({
+                hasToken: token !== null,
+                isLoaded: true
+            });
+        });
+    }
+    render() {
+        if (!this.state.isLoaded) {
+            return ( <
+                ActivityIndicator / >
+            )
+        } else {
+            return ( <
+                Router >
+                <
+                Scene key = 'root' >
+                <
+                Scene component = {
+                    Authentication
+                }
+                //hideNavBar={true}
+                initial = {!this.state.hasToken
+                }
+                key = 'Authentication'
+                title = 'Authentication' /
+                >
+                <
+                Scene component = {
+                    HomePage
+                }
+                hideNavBar = {
+                    true
+                }
+                initial = {
+                    this.state.hasToken
+                }
+                key = 'HomePage'
+                title = 'Home Page' /
+                >
+                <
+                /Scene> <
+                /Router>
+            );
+        }
+    }
 }
